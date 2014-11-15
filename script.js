5c15f9b29aefc7353208b064cd5dfc490f609d72
Andrew Codispoti <andrewcod749@gmail.com>
2014-11-15 01:26:04 -0500

    Fixed site layout to the point where it isn't embarrasing.(except the photo)

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	//handles the image carousel
 	var options = { $AutoPlay: true };
   var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+	$("#slider1_container").width("100%");
 	//handles the button click to scroll the page
 	$(".menubutton").on("click",function (event){
 		//code for the event
@@ -13,12 +14,22 @@ $(document).ready(function(){
 				clickHandler("#contact");
 				break;
 			case "homebutton":
-				clickHandler("#");
+				clickHandler("#home");
 				break;
 
 		}
 	});
+	$("#contactfab").click(function(){
+			$(".contactcardlayout").toggleClass("contacthide");
+
+	});
+	$("#emailcell").click(function(){
+		window.location.href = "mailto:andrewcod749@gmail.com";
+
+
+	});
 	function clickHandler(id){
-		$('html, body').animate({scrollTop: $(id).offset().top}, 2000);
+		console.log(id);
+		$('html, body').animate({scrollTop: $(id).offset().top-100}, 2000);
 	}
 });
