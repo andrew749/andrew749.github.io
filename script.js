010a70e8a70093728ee01d8012cadd958cce8dcb
Andrew Codispoti <andrewcod749@gmail.com>
2014-11-15 00:48:06 -0500

    Added scrolling on page, carousel of images, new assets

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-	$('.homepagemain').mouseover(function(){
-		(this).css("background-color","red");
+	//handles the image carousel
+	var options = { $AutoPlay: true };
+  var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+	//handles the button click to scroll the page
+	$(".menubutton").on("click",function (event){
+		//code for the event
+		switch (event.delegateTarget.id){
+			case "aboutbutton":
+				clickHandler("#about");
+				break;
+			case "contactbutton":
+				clickHandler("#contact");
+				break;
+			case "homebutton":
+				clickHandler("#");
+				break;
+
+		}
 	});
-	
+	function clickHandler(id){
+		$('html, body').animate({scrollTop: $(id).offset().top}, 2000);
+	}
 });
