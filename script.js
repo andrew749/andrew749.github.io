9f7c309f79562ec67762dd4384c6311e0cfd345e
Andrew Codispoti <andrewcod749@gmail.com>
2014-12-18 12:46:00 -0500

    Fixed some positioning aspects of the page.

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
 $(document).ready(function(){
-	//handles the image carousel
-	var options = { $AutoPlay: true };
-  var jssor_slider1 = new $JssorSlider$('slider1_container', options);
-	$("#slider1_container").width("512px");
-	//handles the button click to scroll the page
-	$(".menubutton").on("click",function (event){
-		//code for the event
-		switch (event.delegateTarget.id){
-			case "aboutbutton":
-				clickHandler("#about");
-				break;
-			case "contactbutton":
-				clickHandler("#contact");
-				break;
-			case "homebutton":
-				clickHandler("#home");
-				break;
+    //handles the image carousel
+    var options = { $AutoPlay: true };
+    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+    $("#slider1_container").width("512px");
+    //handles the button click to scroll the page
+    $(".menubutton").on("click",function (event){
+        //code for the event
+        switch (event.delegateTarget.id){
+            case "aboutbutton":
+                clickHandler("#about");
+                purgeClass();
 
-		}
-	});
-	$("#contactfab").click(function(){
-			$(".contactcardlayout").toggleClass("contacthide");
+                $("#aboutbutton").addClass("selectednavbutton");
 
-	});
-	$("#emailcell").click(function(){
-		window.location.href = "mailto:andrewcod749@gmail.com";
+                break;
+            case "contactbutton":
+                clickHandler("#contact");
+                purgeClass();
+
+                $("#contactbutton").addClass("selectednavbutton");
+
+                break;
+            case "homebutton":
+                clickHandler("#home");
+                purgeClass();
+
+                $("#homebutton").addClass("selectednavbutton");
+
+                break;
+
+        }
+    });
+    $("#contactfab").click(function(){
+        $(".contactcardlayout").toggleClass("contacthide");
+
+    });
+    $("#emailcell").click(function(){
+        window.location.href = "mailto:andrewcod749@gmail.com";
 
 
-	});
-	function clickHandler(id){
-		console.log(id);
-		$('html, body').animate({scrollTop: $(id).offset().top-100}, 2000);
-	}
+    });
+    function clickHandler(id){
+        console.log(id);
+        $('html, body').animate({scrollTop: $(id).offset().top-100}, 2000);
+    }
+    function purgeClass(){
+        $("#aboutbutton").removeClass("selectednavbutton");
+        $("#contactbutton").removeClass("selectednavbutton");
+        $("#homebutton").removeClass("selectednavbutton");
+
+
+    }
 });
