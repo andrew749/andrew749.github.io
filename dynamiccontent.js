26282f7378f69173cbba83d1e21a4b7bab40be96
Andrew Codispoti <andrewcod749@gmail.com>
2015-01-10 01:49:01 -0500

    Drew ui in javascript and implemented animations for project cards.

--- dynamiccontent.js
+++ dynamiccontent.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+        var items = [];
+    var waypoint=[];
+    addWaypoint(document.getElementById("textspam"));
     var projectids=["play","heartratemonitor","textmetrics","secretsauce","myomove","hive","scribblerplaystwitch"];
     //spec for entry
     function Entry(title, description, url,subheading){
@@ -7,22 +10,57 @@ $(document).ready(function(){
         this.url=url;
         this.subheading=subheading;
     }
-    var items = [];
 
-    //inflate the template and append to page
-    for (var i in projectids){
-        $.ajax({
-            type: "GET",
-            url: "content/"+projectids[i],
-            dataType: "json" ,
-            success: function (data) {
-                console.log(data);
-                var element="<div class=\"projectview col-md-5 \" id=\""+data.title.toLowerCase()+"\"><h1>"+data.title+"</h1><h3>"+data.subheading+"</h3><br /><br /><div class=\"imagewrapper\"><img class=\"projectimage\" src=\""+data.url+"\"/></div><div class=\"projectdescription\"><p>"+data.description+"</p></div></div>"
-                items.push(element);
-                $('#stuff').append(element);
+    function generateContent(){
+        //inflate the template and append to page
+        for (var i in projectids){
+            $.ajax({
+                type: "GET",
+                url: "content/"+projectids[i],
+                dataType: "json" ,
+                success: function (data) {
+                    console.log(data);
+                    var projectView=$("<div/>");
+                    $(projectView).addClass("projectview col-md-5");
+                    projectView.id=data.title.toLowerCase();
+                    var subHeading=$('<h3/>').text(data.subheading);
+                    var description=$("<p/>").text(data.description);
+                    var descriptionWrapper=$("<div/>");
+                    $(descriptionWrapper).append(description);
+                    $(descriptionWrapper).addClass("projectdescription");
+                    var image=$("<img/>").addClass("projectimage");
+                    var imagewrapper=$("<div/>").addClass("imagewrapper");
+                    $(image).attr("src",data.url);
+                    $(imagewrapper).append(image);
 
-            }
-        });
+                    var title=$("<h1/>").text(data.title);
+                    $(projectView).append(title);
+                    $(projectView).append(subHeading);
+                    $(projectView).append(imagewrapper);
+                    $(projectView).append(descriptionWrapper);
+                    items.push(projectView);
+                    $('#stuff').append(projectView);
+                    addWaypoint(projectView);
+                }
+            });
+        }
     }
+    generateContent();
+    function addWaypoint(element){
+        $(element).addClass("hidecard");
+        var tempwaypoint = new Waypoint({
+            element: element,
+            handler: function() {
+                console.log("addingclass");
+                $(this.element).removeClass("hidecard");
+                $(this.element).addClass("animation");
+            }
+            ,
+            offset:'70%'
+        });
+
+        waypoint.push(tempwaypoint);
+    };
+
 
 });
--- scriptprojects.js
+++ scriptprojects.js
@@ -1,21 +1,21 @@
-//$(document).ready(function(){
-//    var waypoint=[];
-//    var projectids=["textspam","play","heartratemonitor","textmetrics","secretsauce","myounlock","hive","scribblerplaystwitch"];
-//    projectids.forEach(function(entry){
-//        var ele=document.getElementById(entry);
-//        $(ele).addClass("hidecard");
-//        var tempwaypoint = new Waypoint({
-//            element: ele,
-//            handler: function() {
-//                console.log("addingclass");
-//                $(this.element).removeClass("hidecard");
-//                $(this.element).addClass("animation");
-//            }
-//            ,
-//            offset:'70%'
-//        });
-//
-//        waypoint.push(tempwaypoint);
-//    });
-//
-//});
+$(document).ready(function(){
+    var waypoint=[];
+    var projectids=["textspam","play","heartratemonitor","textmetrics","secretsauce","myounlock","hive","scribblerplaystwitch"];
+    projectids.forEach(function(entry){
+        var ele=document.getElementById(entry);
+        $(ele).addClass("hidecard");
+        var tempwaypoint = new Waypoint({
+            element: ele,
+            handler: function() {
+                console.log("addingclass");
+                $(this.element).removeClass("hidecard");
+                $(this.element).addClass("animation");
+            }
+            ,
+            offset:'70%'
+        });
+
+        waypoint.push(tempwaypoint);
+    });
+
+});
