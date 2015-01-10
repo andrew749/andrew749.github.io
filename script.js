50c3c9ef07bae6e9b0d9255d633f6116ebc50e8c
Andrew Codispoti <andrewcod749@gmail.com>
2015-01-10 02:10:01 -0500

    Added slide in animations to homepage.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     //handles the image carousel
     var options = { $AutoPlay: true };
+    var waypoints=[];
     var jssor_slider1 = new $JssorSlider$('slider1_container', options);
     $("#slider1_container").width("100%");
     //handles the button click to scroll the page
@@ -51,22 +52,44 @@ $(document).ready(function(){
 
 
     }
-     //responsive code begin
-        //you can remove responsive code if you don't want the slider scales
-        //while window resizes
-        function ScaleSlider() {
-            var parentWidth = $('#slider1_container').parent().width();
-            if (parentWidth) {
-                       jssor_slider1.$ScaleWidth(parentWidth/2);
-               
-                
-            }
-            else
-                window.setTimeout(ScaleSlider, 30);
-        }
+    //responsive code begin
+    //you can remove responsive code if you don't want the slider scales
+    //while window resizes
+    function ScaleSlider() {
+        var parentWidth = $('#slider1_container').parent().width();
+        if (parentWidth) {
+            jssor_slider1.$ScaleWidth(parentWidth/2);
 
-        //Scale slider while window load/resize/orientationchange.
-        $(window).bind("resize", ScaleSlider);
-        $(window).bind("orientationchange", ScaleSlider);
-        //responsive code end
+
+        }
+        else
+            window.setTimeout(ScaleSlider, 30);
+    }
+
+    //Scale slider while window load/resize/orientationchange.
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    //responsive code end
+
+
+
+    function slideInInformation(element){
+        $(element).addClass("hidecard");
+        var tempwaypoint = new Waypoint({
+            element: element,
+            handler: function() {
+                $(this.element).removeClass("hidecard");
+                $(this.element).addClass("animation_slide_in");
+            }
+            ,
+            offset:'60%'
+        });   
+        waypoints.push(tempwaypoint);
+    }
+    var sections=document.getElementsByClassName('mainsection');
+    for(var i in sections){
+        slideInInformation(sections[i]);
+        console.log(i);
+    }
+
 });
