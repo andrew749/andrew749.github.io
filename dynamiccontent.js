f875a10fa64712dc65004809ddbdb9fa8e4203a9
Andrew Codispoti <andrewcod749@gmail.com>
2015-01-09 23:30:44 -0500

    fixed scribbler bot entry.

--- dynamiccontent.js
+++ dynamiccontent.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
-    var projectids=["play","heartratemonitor","textmetrics"/*,"secretsauce","myounlock","hive","scribblerplaystwitch"*/];
-
-    function Entry(title, description, image){
+    var projectids=["play","heartratemonitor","textmetrics","secretsauce","myomove","hive","scribblerplaystwitch"];
+    //spec for entry
+    function Entry(title, description, image,subheading){
         this.title=title;
         this.description=description;
         this.image=image;
+        this.subheading=subheading;
     }
-    //inflate the template and append to page
-    for (var i in projectids){
-        $.ajax({
-            type: "GET",
-            url: "content/"+projectids[i],
-            dataType: "json" ,
-            success: function (data) {
-                console.log(data);
-                var items = [];
-                items.push( "<li id='" + data.title + "'>" + data.description + "</li>" );
-                $( "<ul/>", {
-                    "class": "my-new-list",
-                    html: items.join( "" )
-                }).appendTo( "body" );
-            }
-        });
-    }
+        //inflate the template and append to page
+        for (var i in projectids){
+            $.ajax({
+                type: "GET",
+                url: "content/"+projectids[i],
+                dataType: "json" ,
+                success: function (data) {
+                    console.log(data);
+                    var items = [];
+                    items.push( "<li id='" + data.title + "'>" +data.title+"<br>Subheading:"+data.subheading+"<br> Description:"+ data.description + "</li>" );
+                    $( "<ul/>", {
+                        "class": "my-new-list",
+                        html: items.join( "" )
+                    }).appendTo( "body" );
+                }
+            });
+        }
 
-});
+    });
