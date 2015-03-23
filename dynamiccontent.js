ccf5eaa95a1fa0ac6794afa7a9f0f68a685c7e60
Andrew Codispoti <andrewcod749@gmail.com>
2015-03-22 21:12:18 -0400

    Added two new projects that I refreshed.

--- dynamiccontent.js
+++ dynamiccontent.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
         var items = [];
     var waypoint=[];
     addWaypoint(document.getElementById("textspam"));
-    var projectids=["play","heartratemonitor","textmetrics","secretsauce","myomove","hive","scribblerplaystwitch"];
+    var projectids=["play","heartratemonitor","textmetrics","secretsauce","myomove","hive","scribblerplaystwitch","decisions", "0xFACE"];
     //spec for entry
     function Entry(title, description, url,subheading){
         this.title=title;
