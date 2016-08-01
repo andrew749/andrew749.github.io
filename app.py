dafeb48d7bdab44139854ae91855ece960337beb
Andrew Codispoti <andrewcod749@gmail.com>
2016-08-01 01:20:42 -0400

    Fixed some rendering.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for, render_template
+import db_helper
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ def index():
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html', title="Projects")
+    return render_template('projects.html', title="Projects", projects=db_helper.getProjects())
 
 @app.route('/resume')
 def resume():
