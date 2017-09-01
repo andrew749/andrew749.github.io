60777ac44d6f75f6dc2911fb0d951f3e9032c80d
Andrew Codispoti <andrewcod749@gmail.com>
2017-09-01 17:33:14 -0400

    Update test.

--- application/test/test_markdown_object.py
+++ application/test/test_markdown_object.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import shutil
 
 from application import markdown_object
 
@@ -15,16 +16,22 @@ sample_markdown = \
 class MarkdownObjectTest(unittest.TestCase):
 
 	def setUp(self):
-		pass
+		# temporary folder to hold intermittent operations
+		os.mkdir("tmp/")
 
 	def tearDown(self):
-		pass
+		shutil.rmtree("tmp/")
 
 	def test_getMarkdownFrontMatter(self):
 		data = markdown_object._getMarkdownFrontMatter(sample_markdown)
 		self.assertEqual(data["title"], "Test title")
 		self.assertEqual(data["subtitle"], "Test subtitle")
 		self.assertEqual(data["date"], "Test date")
+
+	def test_generateHTMLFileFromMarkdown(self):
+		markdown_object._generateHTMLFileFromMarkdown("projects/chordi.json", "tmp/chordi.html")
+		self.assertGreater(os.path.getsize("tmp/chordi.html"), 0)
+		self.assertTrue(os.path.exists("tmp/chordi.html"))
 
 if __name__ == "__main__":
 	unittest.main()
