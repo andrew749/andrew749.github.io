e8805c5ba7d4a79a67b6d4fedec4857451062a64
Andrew Codispoti <andrewcod749@gmail.com>
2017-09-01 22:36:21 -0400

    Fixed constants spacing.

--- application/db_constants.py
+++ application/db_constants.py
@@ -1,26 +1,26 @@
 import os
 
-content_path    = os.path.join(os.path.dirname(__file__),'build/content.sqlite')
+content_path    = os.path.join(os.path.dirname(__file__), 'build/content.sqlite')
 
-content_dir  = 'content'
-blog_dir     = 'blog'
-notes_dir    = 'notes'
+content_dir = 'content'
+blog_dir = 'blog'
+notes_dir = 'notes'
 
 """
 Table for projects
 """
-project_table    = 'projects'
-project_title    = 'title'
+project_table = 'projects'
+project_title = 'title'
 project_subtitle = 'subtitle'
-project_content  = 'content'
-project_image    = 'image_path'
+project_content = 'content'
+project_image = 'image_path'
 
 """
 Table for Blog posts
 """
-blog_table    = 'blog'
-blog_title    = 'title'
+blog_table = 'blog'
+blog_title = 'title'
 blog_subtitle = 'subtitle'
-blog_content  = 'content'
-blog_date     = 'date'
+blog_content = 'content'
+blog_date = 'date'
 
