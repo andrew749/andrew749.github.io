6235d01d54f9425aa3d7a99d4358cc79f8b56520
Andrew Codispoti <andrewcod749@gmail.com>
2017-09-02 01:32:26 -0400

    Added 500 page.

--- app.py
+++ app.py
@@ -60,3 +60,9 @@ def page_not_found(e):
     return render_template(
       '404.html'
     ) 
+
+@application.errorhandler(500)
+def internal_server_error(e):
+  return render_template(
+    '500.html'
+    )
